3607273386b95fa56adf1416c39cde2faf7a5dd2
GeorgiaEm <georgia.mushe@gmail.com>
2017-10-01 20:14:25 +0200

    test

--- tests/testthat/test_val2014.R
+++ tests/testthat/test_val2014.R
@@ -9,8 +9,8 @@ test_that("Assignment: val2014()", {
   expect_is(output[[2]], class = "data.frame", info = "The first data set in output is not a data.frame")
   expect_is(output[[3]], class = "data.frame", info = "The first data set in output is not a data.frame")
   expect_true(any(colnames(output[[1]])=="KOMMUN"), info = "There is no column named 'KOMMUN' in the first data set")
-  expect_true(any(colnames(output[[1]])=="KOMMUN"), info = "There is no column named 'KOMMUN' in the first data set")
-  expect_true(any(colnames(output[[1]])=="KOMMUN"), info = "There is no column named 'KOMMUN' in the first data set")
+  expect_true(any(colnames(output[[2]])=="KOMMUN"), info = "There is no column named 'KOMMUN' in the first data set")
+  expect_true(any(colnames(output[[3]])=="KOMMUN"), info = "There is no column named 'KOMMUN' in the first data set")
   
 })
 
