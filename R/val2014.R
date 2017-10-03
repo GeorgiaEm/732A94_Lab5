f08e33262679a8ca43a9022cf0d07f1dd655dea3
GeorgiaEm <georgia.mushe@gmail.com>
2017-10-04 00:38:53 +0200

    ascii

--- R/val2014.R
+++ R/val2014.R
@@ -12,7 +12,16 @@ val2014<- function(){
   hejsan <- paste("http://www.val.se/val/val2014/statistik/",res,sep="")
   zz<-list()
   for (i in 1:length(hejsan)){
-    zz[[i]]<-read.csv2(as.character(hejsan)[i])
+    data_set<-read.csv2(as.character(hejsan)[i])
+    data_set[,3:4] <- data.frame(lapply(data_set, as.character), stringsAsFactors=FALSE)[,3:4]
+    
+    for (j in 1:nrow(data_set)){
+      for (k in 3:4){
+        data_set[j,k]<-stri_trans_general(data_set[j,k], "latin-ascii")
+        
+      }
+    }
+    zz[[i]]<-data_set
     colnames(zz[[i]])<-stri_trans_general(colnames(zz[[i]]), "latin-ascii")
     
   }
@@ -22,6 +31,6 @@ val2014<- function(){
   Kommunval<<-zz[[3]]
   return(zz)
 }
-#hej<-val2014()
+hej<-val2014()
 #runGitHub("732A94_Lab5_shiny","GeorgiaEm")
 
