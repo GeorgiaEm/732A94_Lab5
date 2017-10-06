7067aa2c3f523b202f0ca1fe88a09e7ef888b5ef
GeorgiaEm <georgia.mushe@gmail.com>
2017-10-06 13:47:31 +0200

    xls

--- R/val2014.R
+++ R/val2014.R
@@ -7,14 +7,29 @@ val2014<- function(){
   require(stringr)
   require(stringi)
   require(ggplot2)
+  require(readxl)
+  require(xlsx)
+  require(httr)
   url <- "http://www.val.se/val/val2014/statistik/index.html"
   get_url <- getHTMLLinks(url)
-  res <- get_url[str_sub(get_url, start= -10)=="kommun.skv"]
+  res1 <- get_url[str_sub(get_url, start= -10)=="kommun.xls"]
+  res2 <- get_url[str_sub(get_url, start= -11)=="kommun.xlsx"]
+  res <- c(res1,res2)
   hejsan <- paste("http://www.val.se/val/val2014/statistik/",res,sep="")
   zz<-list()
   for (i in 1:length(hejsan)){
-    zz[[i]]<-read.csv2(as.character(hejsan)[i])
-    colnames(zz[[i]])<-stri_trans_general(colnames(zz[[i]]), "latin-ascii")
+    if(str_sub(hejsan[i], start= -4)==".xls"){
+      GET(hejsan[i], write_disk(dat <- tempfile(fileext = ".xls")))
+      zz[[i]] <- read_excel(dat, 1L,skip = 2)
+    }
+
+    if(str_sub(hejsan[i], start= -5)==".xlsx"){
+      GET(hejsan[i], write_disk(dat <- tempfile(fileext = ".xlsx")))
+      zz[[i]] <- read_excel(dat, 1L,skip = 2)
+    }
+    
+    # zz[[1]]<-read_excel(hejsan[1])
+    # colnames(zz[[i]])<-stri_trans_general(colnames(zz[[i]]), "latin-ascii")
     
   }
   zz<-list("Riksdagsval"=zz[[1]][1:20],"Landstingsval"=zz[[2]][1:20],"Kommunval"=zz[[3]][1:20])
